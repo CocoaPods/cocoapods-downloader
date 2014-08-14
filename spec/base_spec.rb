0ad4732cf9847b9f49e82fbf1f5cac4a19e209de
Fabio Pelosin <fabiopelosin@gmail.com>
2014-08-14 17:40:03 +0200

    [Specs] Spelling

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path('../spec_helper', __FILE__)
 module Pod
   module Downloader
     describe Base do
-      it 'checks for unrecognize options on initialization' do
+      it 'checks for unrecognized options on initialization' do
         options = { :unrecognized => 'value' }
         e = lambda { Base.new('path', 'url', options) }.should.raise DownloaderError
         e.message.should.match /Unrecognized options/
